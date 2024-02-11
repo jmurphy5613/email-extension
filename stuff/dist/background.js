0db687a5f91a2f4f72d4064761ddb56f9fd0e8de
jmurphy5613 <jmurphy5613@gmail.com>
2024-02-10 21:38:51 -0500

    fixed untracked files

---
+++ stuff/dist/background.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('This is background service worker - edit me!');
+
+/******/ })()
+;
+//# sourceMappingURL=background.js.map
